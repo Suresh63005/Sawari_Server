cd2b5b96f8cd7e7c1c0ca94c8084f4c086549f13
NARENDARKOREM <narendar.korem@innoitlabs.com>
2025-07-21 11:21:56 +0530

    implemeted sub- package apis

---
+++ src/controllers/admin/subPackage.controller.js
@@ -0,0 +1,48 @@
+const subPackageService = require("../../services/subPackage.service");
+
+// Create or Update
+const upsertSubPackage = async (req, res) => {
+  try {
+    const result = await subPackageService.upsertSubPackage(req.body);
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// Get All
+const getAllSubPackages = async (req, res) => {
+  try {
+    const result = await subPackageService.getAllSubPackages(req.query);
+    res.status(200).json({message:"Sub-Packages retrieved successfully",result});
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// Get by ID
+const getSubPackageById = async (req, res) => {
+  try {
+    const result = await subPackageService.getSubPackageById(req.params.id);
+    res.status(200).json({ data: result });
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
+
+// Delete by ID
+const deleteSubPackageById = async (req, res) => {
+  try {
+    const result = await subPackageService.deleteSubPackageById(req.params.id);
+    res.status(200).json({message:"Sub-Package Deleted successfully.",result});
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  upsertSubPackage,
+  getAllSubPackages,
+  getSubPackageById,
+  deleteSubPackageById,
+};
