18789ae82e45acecc34ce0942caf68fd20d70403
diddiakhil <akhil.diddi@innoitlabs.com>
2025-07-31 10:52:51 +0530

    Changes

--- src/api/v1/admin/notifications.routes.js
+++ src/api/v1/admin/notifications.routes.js
@@ -7,5 +7,5 @@ const authMiddleware = require('../../../middlewares/admin/authMiddleware');
 router.post("/sent",authMiddleware.authMiddleware,upload.single("image"),handleMulterError,notificationController.sendNotificationController);
 router.get("/all",notificationController.getAllNotificationsController);
 router.get("/view/:id",notificationController.getSingleNotificationController)
-
+router.delete("/delete/:id",authMiddleware.authMiddleware,notificationController.deleteNotificationController);
 module.exports = router;
